f2a4bf6a28c20476049246f3cba49f65fb4e58cd
MIKA-OGINO-8008 <wakemi.0821@gmail.com>
2025-02-17 18:06:15 +0900

    fix:test_table、bicycles、users の削除に関する変更を記録

--- backend/alembic/versions/6300082117bf_drop_test_table.py
+++ backend/alembic/versions/6300082117bf_drop_test_table.py
@@ -13,7 +13,8 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '6300082117bf'
-down_revision: Union[str, None] = '150eda064af6'
+#down_revision: Union[str, None] = '150eda064af6'
+down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
