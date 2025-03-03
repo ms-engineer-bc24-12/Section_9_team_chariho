c3a583c64f284715381bdb1006a4d98e0a13cb36
haruka <abcute1234@gmail.com>
2025-03-03 10:13:35 +0900

    fix:デモ動画における修正

--- frontend/src/app/mypage/history/page.tsx
+++ frontend/src/app/mypage/history/page.tsx
@@ -11,13 +11,20 @@ export default function HistoryPage() {
         <br />
         {/* 自転車を貸した場合 */}
         <div className="flex flex-col gap-2 mt-4 md-6">
-          <p className="text-2xl font-semibold text-center">売上金：180円</p>
+          <p className="text-2xl font-semibold text-center">売上金：0円</p>
+          <p className="text-2xl font-semibold text-center">お支払金額：1000円</p>
           <br />
           <div className="border p-4 rounded-md w-80">
-            <p>📅 2025/01/10</p>
-            <p>🚲 自転車:マウンテンバイク</p>
-            <p>📍 場所:春日部市</p>
-            <p>💰 料金:500円</p>
+            <p>📅 2025/02/27</p>
+            <p>🚲 自転車の名前:自転車</p>
+            <p>⌚ 予約時間:9:00～12:00</p>
+            <p>💰 料金:300円</p>
+          </div>
+          <div className="border p-4 rounded-md w-80">
+            <p>📅 2025/01/30</p>
+            <p>🚲 自転車の名前:マウンテンバイク</p>
+            <p>⌚ 予約時間:7:00～14:00</p>
+            <p>💰 料金:700円</p>
           </div>
         </div>
         <div className="mt-6">
